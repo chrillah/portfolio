dbc351ab1ea585ad50d35237f0ac10419711a212
chrillah <ch.ronnberg@gmail.com>
2023-02-27 15:08:19 +0100

    nav-bar updated

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,26 @@
 <template>
   <nav class="nav-bar">
     <button class="logo-container">LOGO</button>
-    <ul class="link-container">
-      <li><RouterLink to="/">Hem</RouterLink></li>
-      <li><RouterLink to="/about">Om</RouterLink></li>
-      <li><RouterLink to="/work">Jobb</RouterLink></li>
+    <ul class="links-container">
+      <li class="link-item"><RouterLink class="link" to="/">Hem</RouterLink></li>
+      <li class="link-item"><RouterLink class="link" to="/about">Om</RouterLink></li>
+      <li class="link-item"><RouterLink class="link" to="/work">Jobb</RouterLink></li>
     </ul>
   </nav>
+  <RouterView />
 </template>
+<style>
+.links-container{
+  display: grid;
+  grid-template-columns: 1fr;
+}
+.link-item{
+  text-align: center;
+  padding: 3rem;
+  border: var(--border);
+  color: var(--primary-color);
+}
+.link{
+  text-decoration: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import '../assets/main.css'
 
-createApp(App).use(router).use(store).use(bootstrap).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
