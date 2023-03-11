d6f6a2561dd3aa43b3fc8070c07666a89418bb4d
chrillah <ch.ronnberg@gmail.com>
2023-03-11 20:39:01 +0100

    list of work experience added

---
+++ src/components/WorkExperience.vue
@@ -0,0 +1,40 @@
+<script>
+  export default {
+    data() {
+      return {
+        list: [
+          {
+            year: '2020 - 2022',
+            title: 'Hässelby Villastad Skola',
+            description: 'Teacher of Swedish, English and Mathematics'
+          },
+          {
+            year: '2017 - 2019',
+            title: 'Vittraskolorna AB',
+            description: 'Substitute teacher in art & crafts'
+          },
+          {
+            year: '2016 - 2017',
+            title: 'ND Foto AB',
+            description: 'Graphic design, photo assistant'
+          },
+          {
+            year: '2016',
+            title: 'BASid',
+            description: 'AD assistant, film, photo & graphic production'
+          },
+          {
+            year: '2015',
+            title: 'Stockholms Filmfestival',
+            description: 'Layout coordinator, graphic design'
+          },
+          {
+            year: '2004 - 2014',
+            title: 'Postnord',
+            description: 'Value manager, ISU, address management'
+          }
+        ]
+      }
+    }
+  }
+</script>
